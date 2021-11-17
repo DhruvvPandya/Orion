776d8ee365065bd5205c522f04fb836912ee8f8d
DhruvvPandya <pdhruv.nstack@gmail.com>
2021-11-17 18:42:30 +0530

    thermal printer

--- App.js
+++ App.js
@@ -1,112 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+	View, Text, TouchableOpacity, SafeAreaView
 } from 'react-native';
-
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  BLEPrinter,
+} from "react-native-thermal-receipt-printer";
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+const Printer = () => {
+  const [printers, setPrinters] = useState([]);
+  const [currentPrinter, setCurrentPrinter] = useState();
+
+  useEffect(() => {
+    BLEPrinter.init().then(()=> {
+      BLEPrinter.getDeviceList().then(setPrinters);
+    });
+  }, []);
+
+  connectPrinter = (printer) => {
+    //connect printer
+    BLEPrinter.connectPrinter(printer.inner_mac_address).then(
+      setCurrentPrinter,
+      error => console.warn(error))
+  } 
+
+  printTextTest = () => {
+    currentPrinter && BLEPrinter.printText("<C>Ios Print</C>\n");
+  }
+
+  printBillTest = () => {
+    currentPrinter && BLEPrinter.printBill("<C>Hello Nstack</C>");
+  }
+console.log('printers', printers && printers, currentPrinter && currentPrinter)
+	return (
+    <SafeAreaView style={{flex: 1}}>
+      <View>
+    {
+   printers &&  printers?.map(printer => (
+<TouchableOpacity  onPress={() => connectPrinter(printer)}>
+<Text>{`device_name: ${printer.device_name}, inner_mac_address: ${printer.inner_mac_address}`}</Text>
+</TouchableOpacity>
+        ))
+    }
     </View>
-  );
+    <TouchableOpacity onPress={printTextTest}>
+      <Text>Nstack Hello</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={printBillTest}>
+      <Text>Print</Text>
+    </TouchableOpacity>
+  </SafeAreaView>
+	);
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default Printer;
